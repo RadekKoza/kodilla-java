957dcacc59e0e284881b4afced3c23a2044fd8e7
RadekKoza <radekk1@poczta.fm>
2025-05-29 11:59:00 +0200

    Zad.12.1 funkcyjna iteracja po tytułach - poprawka

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/TitlesStream.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/TitlesStream.java
@@ -1,10 +1,13 @@
 package com.kodilla.good.patterns.challenges;
 
+import java.util.stream.Collectors;
+
 public class TitlesStream {
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
-        movieStore.getMovies().entrySet().stream()
-                .flatMap(films -> films.getValue().stream())
-                        .forEach(title -> System.out.print(title + "!"));
+        String filmString = movieStore.getMovies().entrySet().stream()
+                .flatMap(films ->
+                        films.getValue().stream()).collect(Collectors.joining("!"));
+        System.out.println(filmString);
     }
 }
